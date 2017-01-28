87792193fd65b9fe64d29fbbbf592a247effabed
linhsaarnold <linhsa.arnold@gmail.com>
2017-01-27 23:55:26 -0800

    HW4

---
+++ untangling/Homework-4/hm4.js
@@ -0,0 +1,9 @@
+function change(){
+    
+   document.getElementById('myTextField').value;
+       alert('Aw, they are so cute!');
+   
+   var title = document.getElementById('title');
+   title.innerHTML = myNewTitle;
+    
+}
