ff1e2025776ae502d4691f4817851f2f55961434
linhsaarnold <linhsa.arnold@gmail.com>
2017-02-01 20:32:42 -0800

    Inital Clone

--- untangling/Homework-4/hm4.js
+++
@@ -1,9 +0,0 @@
-function change(){
-    
-   document.getElementById('myTextField').value;
-       alert('Aw, they are so cute!');
-   
-   var title = document.getElementById('title');
-   title.innerHTML = myNewTitle;
-    
-}
